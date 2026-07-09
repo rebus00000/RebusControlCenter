9ebddb3f7d3ed7525d3034437d1643e26442adf0
𝗥𝗘𝗕𝗨𝗦 <152723895+rebus00000@users.noreply.github.com>
2026-07-09 22:53:21 +0530

    Add ControlCenterApp

---
+++ app/src/main/java/com/rebus/controlcenter/ControlCenterApp.kt
@@ -0,0 +1,15 @@
+package com.rebus.controlcenter
+
+import android.app.Application
+
+class ControlCenterApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Global initialization
+        // Theme
+        // Settings
+        // Overlay Manager
+    }
+}
