a6c371f0fd589e6c01fb907b17e41cc9605e2e3c
𝗥𝗘𝗕𝗨𝗦 <152723895+rebus00000@users.noreply.github.com>
2026-07-09 22:59:09 +0530

    Add color definitions for UI theme

---
+++ app/src/main/java/com/rebus/controlcenter/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.rebus.controlcenter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF4F8CFF)
+val DarkBlue = Color(0xFF1A2B50)
+
+val Background = Color(0xFF101114)
+val Card = Color(0xFF1E1F25)
+val Accent = Color(0xFF00E5FF)
